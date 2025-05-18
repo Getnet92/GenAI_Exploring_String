55f1bf986540471a5b6a8ed065fcc01a01cbde39
Getnet92 <Gworkale@terpmail.umd.edu>
2025-05-18 18:49:06 -0400

    Update GenAI_Exploring_String to include task 3

--- GenAI_Exploring_String.py
+++ GenAI_Exploring_String.py
@@ -22,3 +22,19 @@ print("The first letter capitalized: " + capital_first)
 # replacing world with universe
 replica_word = (word.replace("world" , "universe")) # using the replace funcution
 print(replica_word)
+
+# Task 3
+palindrome = str(input("Enter your palindrome: ")) # prompting user to enter palindrome
+palindrome = palindrome.strip() # stripping white space if appears
+size = int(len(palindrome)) # taking the size of the string to use for the number of iterations needed
+check = palindrome[::-1]
+n = 0 # to be later used to compare against the size of the string 
+for i in range (size):
+    if ( palindrome[i] == check[i]): # condition to update n to later display message depending on value of n
+        n+=1
+    else:
+        print("This word is not a palindrome")
+        break # breaking if conditions aren't met to stop checking, only 1 letter is needed to be misplaced
+
+if n == size: # only runs if n is equal to size meanding the number of letters in the correct order equal the size of the string meaning the word and it's reverse are the same word
+    print(palindrome + " is a palindrome ")
